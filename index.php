dc9fffdeabec66fdaecc8ee79701732ed9d3c61c
HoangPhuc2707 <tranp8639@gmail.com>
2023-06-16 12:26:45 +0700

    init project

---
+++ index.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Profile</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <video class="back-video" autoplay loop muted plays-inline src="images/video1.mp4"></video>
+        <div class="top">Hoang Phuc</div>
+        <div class="header">
+            <p class="hp">HOANG PHUC</p>
+            <p>PHP Developer</p>
+        </div>
+        <div class="nen-gt">
+            <div class="box-left">
+                <img src="images/img8.jpg" alt="">
+            </div>
+            <div class="box-between">
+                <img src="images/me.png" alt="">
+            </div>
+            <div class="box-right">
+                <img src="images/img3.jpg" alt="">
+            </div>
+        </div>
+        <div class="main">
+            <div class="my-info">
+                <p>My Abilities</p>
+                <h1>Skills</h1>
+            </div>
+            <div class="skill">
+                <ul class="skills">
+                    <li>
+                        <div class="language">
+                            <h2>HTML</h2>
+                            <div class="icon-html">
+                                <img src="images/iconhtml.png" alt="">
+                            </div>
+                            <p>60</p>
+                            <div class="icon-loadbar">
+                                <img src="images/load-bar.png" alt="">
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="language">
+                            <h2>CSS</h2>
+                            <div class="icon-css">
+                                <img src="images/iconcss.png" alt="">
+                            </div>
+                            <p>60</p>
+                            <div class="icon-loadbar">
+                                <img src="images/load-bar.png" alt="">
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="language">
+                            <h2>PHP</h2>
+                            <div class="icon-php">
+                                <img src="images/iconphp.png" alt="">
+                            </div>
+                            <p>60</p>
+                            <div class="icon-loadbar">
+                                <img src="images/load-bar.png" alt="">
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="language">
+                            <h2>JS</h2>
+                            <div class="icon-js">
+                                <img src="images/iconjs.png" alt="">
+                            </div>
+                            <p>60</p>
+                            <div class="icon-loadbar">
+                                <img src="images/load-bar.png" alt="">
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="my-info">
+                <p>My Hobbies</p>
+                <h1>Game</h1>
+            </div>
+            <div class="game">
+                <ul class="games">
+                    <li>
+                        <div class="gamess">
+                            <h2>Liên Quân Mobile</h2>
+                            <div class="icon-html">
+                                <a href="https://lienquan.garena.vn/">
+                                    <img src="images/lq.png" alt="">
+                                </a>
+                            </div>
+                            <div class="img-lq">
+                                <img src="images/imglq.jpg" alt="">
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="gamess">
+                            <h2>OMG 3Q</h2>
+                            <div class="icon-html">
+                                <a href="http://omg3q.vnggames.com/">
+                                    <img src="images/omg.png" alt="">
+                                </a>
+                            </div>
+                            <div class="img-lq">
+                                <img src="images/imgomg.jpg" alt="">
+                            </div>
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
